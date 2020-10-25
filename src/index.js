7823f63290bc5318081cd39f27a3c05cd2111c88
Pavel <browser091@gmail.com>
2020-10-25 20:42:15 +0300

    feat: add morse

--- src/index.js
+++ src/index.js
@@ -37,9 +37,26 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+function decode(message) {
+    let morse_table = new Map(Object.entries(MORSE_TABLE));
+
+    let result = message.match(/[0-1*]{10}/g).map(item => 
+        item.match(/[0-1*]{2}/g).reduce((letter, item) => {
+            switch (item) {
+                case "00":
+                    return letter;
+                case "10":
+                    return letter + ".";
+                case "11":
+                    return letter + "-";
+                default:
+                    return letter + " ";
+            }
+        }, "")
+    );
+    
+    return result.map(item => morse_table.get(item) || " " ).join("");
+ }
 
 module.exports = {
     decode
